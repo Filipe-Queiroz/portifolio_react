bc00cc0e8c8114c2396afe4c516d3d0b8e914611
Filipe Queiroz <filipe.queiroz.312@gmail.com>
2024-09-20 15:22:56 -0300

    configurando o ambiente de desenvolvimento

---
+++ src/styles.ts
@@ -0,0 +1,10 @@
+import { createGlobalStyle } from 'styled-components'
+
+// eslint-disable-next-line no-unused-vars
+const EstiloGlobal = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+  }
+`
+export default EstiloGlobal
