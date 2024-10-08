b5df271c34f6e9f8a1f461c26ea3dc9246a01e90
Filipe Queiroz <filipe.queiroz.312@gmail.com>
2024-10-08 11:13:54 -0300

    estilizando a sidebar

---
+++ src/components/Avatar/index.tsx
@@ -0,0 +1,5 @@
+import { FotoPerfil } from './styles'
+
+const Avatar = () => <FotoPerfil src="https://github.com/Filipe-Queiroz.png" />
+
+export default Avatar
--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,9 +1,22 @@
+import Avatar from '../../components/Avatar'
+import Paragrafo from '../../components/Paragrafo'
 import Titulo from '../../components/Titulo'
+
+import { Descricao, BotaoTema, SidebarContainer } from './styles'
 
 const Sidebar = () => (
   <aside>
-    <img src="https://github.com/Filipe-Queiroz.png" />
-    <Titulo fontSize={20}>Filipe Queiroz</Titulo>
+    <SidebarContainer>
+      <Avatar></Avatar>
+      <Titulo fontSize={20}>Filipe Queiroz</Titulo>
+      <Paragrafo tipo="secundario" font-size={16}>
+        Filipe-Queiroz
+      </Paragrafo>
+      <Descricao tipo="principal" fontSize={12}>
+        Engenheiro front-end
+      </Descricao>
+      <BotaoTema>Trocar tema</BotaoTema>
+    </SidebarContainer>
   </aside>
 )
 
