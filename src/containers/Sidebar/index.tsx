f147d6b6e0506778976702c2dacc3074ab658c36
Filipe Queiroz <filipe.queiroz.312@gmail.com>
2024-10-08 09:29:10 -0300

    construindo estrutura e a seção "sobre mim"

---
+++ src/containers/Sidebar/index.tsx
@@ -0,0 +1,10 @@
+import Titulo from '../../components/Titulo'
+
+const Sidebar = () => (
+  <aside>
+    <img src="https://github.com/Filipe-Queiroz.png" />
+    <Titulo fontSize={20}>Filipe Queiroz</Titulo>
+  </aside>
+)
+
+export default Sidebar
