e7a0105342d180f70660d242c0756bf6fa608bdf
Filipe Queiroz <filipe.queiroz.312@gmail.com>
2024-10-08 09:59:41 -0300

    criando seção de projetos e componente de projetos

--- src/containers/Projetos/index.tsx
+++ src/containers/Projetos/index.tsx
@@ -1,8 +1,37 @@
 import Titulo from '../../components/Titulo'
+import Projeto from '../../components/Projeto'
+
+import { Lista } from './styles'
 
 const Projetos = () => (
   <section>
     <Titulo>Projetos</Titulo>
+    <Lista>
+      <li>
+        <Projeto></Projeto>
+      </li>
+      <li>
+        <Projeto></Projeto>
+      </li>
+      <li>
+        <Projeto></Projeto>
+      </li>
+      <li>
+        <Projeto></Projeto>
+      </li>
+      <li>
+        <Projeto></Projeto>
+      </li>
+      <li>
+        <Projeto></Projeto>
+      </li>
+      <li>
+        <Projeto></Projeto>
+      </li>
+      <li>
+        <Projeto></Projeto>
+      </li>
+    </Lista>
   </section>
 )
 
